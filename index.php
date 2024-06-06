1cbbf79fac10ed1d896971538c024a28603bdde4
Adenis Ahmeti <105243644+adahmeti@users.noreply.github.com>
2024-06-06 05:40:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare (srtict_type = 1);
+
+function robotName(): string{
+$number = mt_rand(100,999)
+$letters = range("A", "B")'
+shuffle = ($letters);
+
+return "{$letters[0]}{$letters[1]}{$number}"
+}
+
+echo robotName();
+
+?>
